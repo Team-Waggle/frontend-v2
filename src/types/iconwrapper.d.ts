1edc87d63e7cf188fd9f81c266f9f1870dc9a75c
Minki <88329887+Min9ithub@users.noreply.github.com>
2026-02-15 17:40:08 +0900

    Revert "feat: IconWrapper 생성"

--- src/types/iconwrapper.d.ts
+++
@@ -1,8 +0,0 @@
-export type IconWrapperColor = 'outline' | 'filled' | 'transparent';
-export type IconWrapperShape = 'square' | 'circle';
-
-export interface IconWrapperProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-  color?: IconWrapperColor;
-  shape?: IconWrapperShape;
-  isSelected?: boolean;
-}
