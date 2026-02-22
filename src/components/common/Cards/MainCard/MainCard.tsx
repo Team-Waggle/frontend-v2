234a4ff37b31e5cb118b248f164ec769db6f0b81
예은 <143199798+b1uey0@users.noreply.github.com>
2026-02-22 20:22:04 +0900

    refactor, design: MainCard를 MainPage 뿐만 아니라 TeamHome에서도 사용할 수 있게 디자인 및 기능 수정

--- src/components/common/Cards/MainCard/MainCard.tsx
+++ src/components/common/Cards/MainCard/MainCard.tsx
@@ -1,32 +1,56 @@
 import MainCardTag from './MainCardTag';
 
 /* 임시 SkillIcon */
-import SkillJava from '../../../assets/icons/skill/large/ic_skill_Java_large.svg?react';
-import SkillFigma from '../../../assets/icons/skill/large/ic_skill_Figma_large.svg?react';
-import SkillDjango from '../../../assets/icons/skill/large/ic_skill_Django_large.svg?react';
-import SkillJavaScript from '../../../assets/icons/skill/large/ic_skill_JavaScript_large.svg?react';
-import SkillTypescript from '../../../assets/icons/skill/large/ic_skill_TypeScript_large.svg?react';
+import SkillJava from '../../../../assets/icons/skill/large/ic_skill_Java_large.svg?react';
+import SkillFigma from '../../../../assets/icons/skill/large/ic_skill_Figma_large.svg?react';
+import SkillDjango from '../../../../assets/icons/skill/large/ic_skill_Django_large.svg?react';
+import SkillJavaScript from '../../../../assets/icons/skill/large/ic_skill_JavaScript_large.svg?react';
+import SkillTypescript from '../../../../assets/icons/skill/large/ic_skill_TypeScript_large.svg?react';
 
-import MeatBallIcon from '../../../assets/icons/skill/large/ic_skill_meatball_large.svg?react';
+import MeatBallIcon from '../../../../assets/icons/skill/large/ic_skill_meatball_large.svg?react';
 
 /**
  * Main Components: MainCard
- * 1. 해당 MainCard는 MainPage에서 사용하는 MainCard
+ * 1. 해당 MainCard는 MainPage와 TeamHomePage에서 사용하는 MainCard입니다.
+ *  1-1. variant로 main, team 을 구분했습니다.
  */
 
 interface MainCardProps {
-  MainCardTitle?: string;
+  mainCardTitle?: string;
+  variant?: 'main' | 'team';
+  isActvie?: boolean;
+  onClick?: () => void;
 }
 
-const MainCard = ({ MainCardTitle }: MainCardProps) => {
+const MainCard = ({
+  mainCardTitle,
+  variant = 'main',
+  isActvie=false,
+  onClick
+}: MainCardProps) => {
+
+  const baseStyle = "pointer-events-auto flex w-[36.8rem] min-w-[33.6rem] cursor-pointer flex-col items-start gap-[3.2rem] rounded-[1.2rem] border border-solid border-[#E7E8EA] bg-white px-[2.8rem] py-[2.4rem]";
+
+  const hoverStyle =
+    variant === 'main' ? 'hover:shadow-main-card' : 'hover:bg-hover-5';
+
+  const activeStyle =
+    isActvie
+      ? "border-blue-70"
+      : "";
+
   return (
-    <div className="flex w-[36.8rem] min-w-[33.6rem] cursor-pointer flex-col items-start gap-[3.2rem] rounded-[1.2rem] border border-solid border-[#E7E8EA] bg-white px-[2.8rem] py-[2.4rem] hover:shadow-main-card">
+    <div
+      data-main-card="true"
+      className={`${baseStyle} ${hoverStyle} ${activeStyle}`}
+      onClick={onClick}
+    >
       {/** Frame 01 */}
       <div className="flex h-[9rem] min-w-[28rem] flex-col items-start gap-[1.6rem] self-stretch">
         {/** MainCard Title */}
         <div className="flex h-[4.6rem] max-h-[4.6rem] flex-shrink-0 items-start gap-[1.2rem] self-stretch overflow-hidden overflow-ellipsis">
           <span className="line-clamp-2 overflow-hidden text-ellipsis text-[1.6rem] font-[600] text-[#0E0E0E]">
-            {MainCardTitle}
+            {mainCardTitle}
           </span>
         </div>
         {/** MainCard Job Tag */}
