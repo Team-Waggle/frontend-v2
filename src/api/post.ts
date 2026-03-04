22676459228d6fa85ed3e7829d9ce507d279f64b
예은 <143199798+b1uey0@users.noreply.github.com>
2026-03-04 23:01:49 +0900

    feat: 모집글 목록 페이지네이션 api 추가

--- src/api/post.ts
+++ src/api/post.ts
@@ -1,4 +1,4 @@
-import { POST_DETAIL_URL } from '../constants/endpoint';
+import { POST_DETAIL_URL, POST_URL } from '../constants/endpoint';
 import axiosInstance from './axiosInstance';
 
 // 모집글 상세조회
@@ -6,3 +6,18 @@ export const GetPostDetail = async (postId: number) => {
   const { data } = await axiosInstance.get(POST_DETAIL_URL(postId));
   return data;
 }
+
+// 모집글 목록 페이지네이션
+// 기본값: 12개씩, sort는 최신순
+export const getPosts = async ({ q, page = 0 }: { q?: string; page?: number } = {}) => {
+    const { data } = await axiosInstance.get(POST_URL, {
+        params: {
+            q,
+            page,
+            size: 4,
+            sort: ["postId,desc"],
+        },
+    });
+
+    return data;
+};
