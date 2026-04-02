6c69ec096d10c0c007f28bc8fd9356a6093208ff
Minki <nam960216@gmail.com>
2026-04-02 17:08:05 +0900

    fix: sidebar 수정

--- src/components/Sidebar/SidebarMenu.tsx
+++ src/components/Sidebar/SidebarMenu.tsx
@@ -1,6 +1,7 @@
 import { useLocation, useNavigate } from 'react-router';
 import SidebarIcon from './SidebarIcon';
 import SidebarItem from './SidebarItem';
+import type { TeamResponse } from '../../types/api/team';
 
 // Icons
 import FaceSmileIcon from '../../assets/icons/normal/ic_faceSmile.svg?react';
@@ -9,7 +10,7 @@ import MessageIcon from '../../assets/icons/normal/ic_message.svg?react';
 import PersonIcon from '../../assets/icons/normal/ic_person.svg?react';
 
 type SidebarPathType = {
-  teamId?: number;
+  teamData: TeamResponse[];
   userId?: string;
 };
 
@@ -25,7 +26,8 @@ const SIDEBAR_MENUS: SidebarMenu[] = [
     key: 'team',
     label: '내팀',
     icon: <FaceSmileIcon />,
-    getPath: ({ teamId }) => `/team/${teamId}`,
+    getPath: ({ teamData }) =>
+      teamData?.[0]?.teamId ? `/team/${teamData[0].teamId}` : '/team/new',
   },
   {
     key: 'notification',
@@ -47,22 +49,17 @@ const SIDEBAR_MENUS: SidebarMenu[] = [
   },
 ];
 
-// const myTeams = [
-//   { id: 1, name: '와글팀' },
-//   { id: 2, name: '일이삼사오육칠팔구' },
-// ];
-
 const SidebarMenu = ({
   isLoggedIn,
   setIsLoginModalOpen,
   isFolded,
-  teamId,
+  teamData,
   userId,
 }: {
   isLoggedIn: boolean;
   setIsLoginModalOpen: () => void;
   isFolded: boolean;
-  teamId?: number;
+  teamData: TeamResponse[];
   userId?: string;
 }) => {
   const { pathname } = useLocation();
@@ -72,12 +69,20 @@ const SidebarMenu = ({
     ? SIDEBAR_MENUS
     : SIDEBAR_MENUS.filter((menu) => menu.key === 'team');
 
+  const teamSubItems = isLoggedIn
+    ? teamData.map((team) => ({
+        teamId: team.teamId,
+        name: team.name,
+        profileImageUrl: team.profileImageUrl,
+      }))
+    : [];
+
   return (
     <div
       className={`flex flex-col gap-[0.4rem] ${isFolded ? 'w-[4.8rem]' : 'w-[25.8rem]'}`}
     >
       {visibleMenus.map(({ key, icon, label, getPath }) => {
-        const path = getPath({ teamId, userId });
+        const path = getPath({ teamData, userId });
 
         const isActive =
           pathname.includes(key) &&
@@ -105,6 +110,7 @@ const SidebarMenu = ({
             icon={icon}
             label={label}
             isActive={isActive}
+            subItems={key === 'team' ? teamSubItems : undefined}
             onClick={handleClick}
           />
         );
