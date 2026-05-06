8697342bfbee9d0e6fa559d66d7f7a4ff3c30e9b
Minki <nam960216@gmail.com>
2026-05-07 03:45:28 +0900

    fix: 라우트 에러 수정

--- src/router.tsx
+++ src/router.tsx
@@ -5,48 +5,75 @@ import {
 } from 'react-router-dom';
 import './styles/global.css';
 import App from './App';
+import {
+  partnerLoader,
+  postLoader,
+  teamLoader,
+  userLoader,
+  withAuth,
+} from './loaders/routeLoaders';
 
 // Pages
+import RouteErrorPage from './pages/RouteErrorPage';
 import MainPage from './pages/MainPage';
-import LoginPage from './pages/LoginPage';
 import TeamFormPage from './pages/TeamFormPage';
 import TeamHomePage from './pages/TeamHomePage';
 import TeamPostManagementPage from './pages/TeamPostManagementPage';
-import PostDetailPage from './pages/PostDetailPage';
-import PostFormPage from './pages/PostFormPage';
-import NotFoundPage from './pages/NotFoundPage';
-import TeamStatusPage from './pages/TeamStatusPage';
-import MyPage from './pages/MyPage';
-import ProfileRedirect from './components/Profile/ProfileRedirect';
-import MessagePage from './pages/MessagePage';
 import TeamApplicantPage from './pages/TeamApplicantPage';
+import TeamStatusPage from './pages/TeamStatusPage';
+import PostFormPage from './pages/PostFormPage';
+import PostDetailPage from './pages/PostDetailPage';
+import ProfileRedirect from './components/Profile/ProfileRedirect';
+import MyPage from './pages/MyPage';
+import MessagePage from './pages/MessagePage';
+import LoginPage from './pages/LoginPage';
+import NotFoundPage from './pages/NotFoundPage';
 
 export const router = createBrowserRouter(
   createRoutesFromElements(
     <>
-      <Route element={<App />}>
+      <Route element={<App />} errorElement={<RouteErrorPage />}>
         <Route path="/" element={<MainPage />} />
-        <Route path="/team/new" element={<TeamFormPage />} />
-        <Route path="/team/:teamId">
+        <Route
+          path="/team/new"
+          element={<TeamFormPage />}
+          loader={withAuth()}
+        />
+        <Route path="/team/:teamId" loader={withAuth(teamLoader)}>
           <Route index element={<TeamHomePage />} />
           <Route path="edit" element={<TeamFormPage />} />
           <Route path="posts" element={<TeamPostManagementPage />} />
           <Route path="applicants" element={<TeamApplicantPage />} />
           <Route path="status" element={<TeamStatusPage />} />
         </Route>
-        <Route path="/post/new" element={<PostFormPage />} />
-        <Route path="/post/:postId" element={<PostDetailPage />} />
-        <Route path="/post/:postId/edit" element={<PostFormPage />} />
-        <Route path="/profile" element={<ProfileRedirect />} />
-        <Route path="/profile/:userId" element={<MyPage />} />
-        <Route path="/message" element={<MessagePage />} />
-        <Route path="/message/:partnerId" element={<MessagePage />} />
+        <Route
+          path="/post/new"
+          element={<PostFormPage />}
+          loader={withAuth()}
+        />
+        <Route path="/post/:postId" loader={postLoader}>
+          <Route index element={<PostDetailPage />} />
+          <Route path="edit" element={<PostFormPage />} loader={withAuth()} />
+        </Route>
+        <Route
+          path="/profile"
+          element={<ProfileRedirect />}
+          loader={withAuth()}
+        />
+        <Route
+          path="/profile/:userId"
+          element={<MyPage />}
+          loader={withAuth(userLoader)}
+        />
+        <Route path="/message" element={<MessagePage />} loader={withAuth()} />
+        <Route
+          path="/message/:partnerId"
+          element={<MessagePage />}
+          loader={withAuth(partnerLoader)}
+        />
       </Route>
       <Route path="/oauth/callback" element={<LoginPage />} />
       <Route path="*" element={<NotFoundPage />} />
     </>,
   ),
 );
-
-// 내팀/모집글 관리 /team/:teamId/posts
-// 메시지 /message
